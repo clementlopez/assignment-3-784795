392bd84ccd201fd3e697f4e6677930690283ea8a
clement.lopez <clement.lopez@moonshotlabs.com>
2019-11-20 18:47:10 +0200

    flink basic

--- code/Storm/storm/mqtt/bolt/Customer1TupleMapper.java
+++
@@ -1,7 +0,0 @@
-public class Customer1TupleMapper implements MqttTupleMapper {
-    public MqttMessage toMessage(ITuple tuple) {
-        String topic = "users/" + tuple.getStringByField("userId");
-        byte[] payload = tuple.getStringByField("message").getBytes();
-        return new MqttMessage(topic, payload);
-    }
-}
--- code/Storm/storm/mqtt/spout/Customer1Spout.java
+++
@@ -1,7 +0,0 @@
-package storm.mqtt.spout;
-
-import org.apache.storm.topology.IRichSpout;
-
-public class Customer1Spout implements IRichSpout, MqttCallback {
-
-}
